75d089b54edbc701dea89e89dfc9a2a091e7bf09
Kilian Käslin <kilian.kaeslin@tum.de>
2021-03-27 15:25:03 +0100

    - Added goerli registry address

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import { sign, hashContract, verify, configureProvider } from './utils';
 import { Attribute, ProviderConfig, NetworkConfig } from './types';
 
-export const REGISTRY = 'xxx';
+export const REGISTRY = '0x90F39Cf6d18f1bB4fe855CbAEB9eC6DFbD3AC0FD';
 
 export { sign, hashContract, verify, configureProvider };
 export { Attribute, ProviderConfig, NetworkConfig };
